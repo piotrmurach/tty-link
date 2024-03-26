769c4b9ca82dc01a8e25f4afd5d1770bf0e6b469
Piotr Murach <pmurach@gmail.com>
2024-03-26 21:23:00 +0000

    Add additional tests for terminal version parsing

--- spec/unit/parse_version_spec.rb
+++ spec/unit/parse_version_spec.rb
@@ -2,11 +2,20 @@
 
 RSpec.describe TTY::Link, "#parse_version" do
   [
-    {from: "4602", to: {major: 0, minor: 46, patch: 2}},
-    {from: "5104", to: {major: 0, minor: 51, patch: 4}},
-    {from: "0.51.0", to: {major: 0, minor: 51, patch: 0}}
+    {from: "1234", to: {major: 0, minor: 12, patch: 34}},
+    {from: "123", to: {major: 0, minor: 1, patch: 23}},
+    {from: "0204", to: {major: 0, minor: 2, patch: 4}},
+    {from: "1030", to: {major: 0, minor: 10, patch: 30}},
+    {from: "12.34.56", to: {major: 12, minor: 34, patch: 56}},
+    {from: "0.1.2", to: {major: 0, minor: 1, patch: 2}},
+    {from: "1.0.", to: {major: 1, minor: 0, patch: nil}},
+    {from: "1.0", to: {major: 1, minor: 0, patch: nil}},
+    {from: "1.", to: {major: 1, minor: nil, patch: nil}},
+    {from: "12", to: {major: 12, minor: nil, patch: nil}},
+    {from: "1", to: {major: 1, minor: nil, patch: nil}},
+    {from: "", to: {major: nil, minor: nil, patch: nil}}
   ].each do |data|
-    it "parses #{data[:from]} to #{data[:to]}" do
+    it "parses #{data[:from].inspect} to #{data[:to]}" do
       expect(described_class.parse_version(data[:from])).to eq(data[:to])
     end
   end
