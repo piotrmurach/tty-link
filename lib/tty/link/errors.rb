51cdbf6b7c252330e00b29da4ebd5e53993e40c2
Piotr Murach <pmurach@gmail.com>
2024-08-18 14:13:18 +0200

    Add TTY::Link::AbstractMethodError class to signal calling an abstract method

--- lib/tty/link/errors.rb
+++ lib/tty/link/errors.rb
@@ -6,5 +6,28 @@ module TTY
     #
     # @api public
     Error = Class.new(StandardError)
+
+    # Raised when an abstract method is called
+    #
+    # @api public
+    class AbstractMethodError < Error
+      MESSAGE = "the %<class_name>s class must implement " \
+                "the `%<method_name>s` method"
+
+      # Create an {TTY::Link::AbstractMethodError} instance
+      #
+      # @example
+      #   TTY::Link::AbstractMethodError.new("Terminal", "name?")
+      #
+      # @param [String] class_name
+      #   the class name
+      # @param [Symbol] method_name
+      #   the method name
+      #
+      # @api private
+      def initialize(class_name, method_name)
+        super(format(MESSAGE, class_name: class_name, method_name: method_name))
+      end
+    end
   end # Link
 end # TTY
