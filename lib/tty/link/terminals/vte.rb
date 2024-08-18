2803de9dab6dd47c45f4c30e9ae37948264b2cae
Piotr Murach <pmurach@gmail.com>
2024-08-18 15:57:10 +0200

    Change the TTY::Link::Terminals::Vte class to inherit from the Abstract class

--- lib/tty/link/terminals/vte.rb
+++ lib/tty/link/terminals/vte.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
+require_relative "abstract"
+
 module TTY
   class Link
     module Terminals
       # Responsible for detecting hyperlink support in the VTE-based terminal
       #
       # @api private
-      class Vte
+      class Vte < Abstract
         # The VTE version environment variable name
         #
         # @return [String]
@@ -14,35 +16,6 @@ module TTY
         # @api private
         VTE_VERSION = "VTE_VERSION"
         private_constant :VTE_VERSION
-
-        # Create an {TTY::Link::Terminals::Vte} instance
-        #
-        # @example
-        #   vte = TTY::Link::Terminals::Vte.new(SemanticVersion, ENV)
-        #
-        # @param [TTY::Link::SemanticVersion] semantic_version
-        #   the semantic version creator
-        # @param [ENV, Hash{String => String}] env
-        #   the environment variables
-        #
-        # @api public
-        def initialize(semantic_version, env)
-          @semantic_version = semantic_version
-          @env = env
-        end
-
-        # Detect VTE hyperlink support
-        #
-        # @example
-        #   vte.link?
-        #   # => true
-        #
-        # @return [Boolean]
-        #
-        # @api public
-        def link?
-          name? && version?
-        end
 
         private
 
@@ -74,30 +47,6 @@ module TTY
           current_semantic_version >= semantic_version(0, 50, 1)
         end
 
-        # Create a {TTY::Link::SemanticVersion} instance from a version value
-        #
-        # @example
-        #   vte.semantic_version(1, 2, 3)
-        #
-        # @example
-        #   vte.semantic_version("1.2.3")
-        #
-        # @param [Array<Integer, String>] version
-        #   the version to convert to a semantic version
-        # @param [Hash{Symbol => String}] options
-        #   the options to convert to a semantic version
-        # @option options [String] :separator
-        #   the version separator
-        #
-        # @return [TTY::Link::SemanticVersion]
-        #
-        # @see SemanticVersion#from
-        #
-        # @api private
-        def semantic_version(*version, **options)
-          @semantic_version.from(*version, **options)
-        end
-
         # Read the VTE version environment variable
         #
         # @example
@@ -108,7 +57,7 @@ module TTY
         #
         # @api private
         def vte_version
-          @env[VTE_VERSION]
+          env[VTE_VERSION]
         end
       end # Vte
     end # Terminals
