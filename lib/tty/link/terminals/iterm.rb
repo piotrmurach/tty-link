3fbe110493f1f97733c918076df04d9253c52350
Piotr Murach <pmurach@gmail.com>
2024-08-24 13:39:01 +0200

    Change TTY::Link::Terminals::Iterm to remove term program and version methods

--- lib/tty/link/terminals/iterm.rb
+++ lib/tty/link/terminals/iterm.rb
@@ -17,22 +17,6 @@ module TTY
         ITERM = /iTerm(\s*\d+){0,1}.app/x.freeze
         private_constant :ITERM
 
-        # The term program environment variable name
-        #
-        # @return [String]
-        #
-        # @api private
-        TERM_PROGRAM = "TERM_PROGRAM"
-        private_constant :TERM_PROGRAM
-
-        # The term program version environment variable name
-        #
-        # @return [String]
-        #
-        # @api private
-        TERM_PROGRAM_VERSION = "TERM_PROGRAM_VERSION"
-        private_constant :TERM_PROGRAM_VERSION
-
         private
 
         # Detect iTerm terminal
@@ -63,32 +47,6 @@ module TTY
           current_semantic_version = semantic_version(term_program_version)
 
           current_semantic_version >= semantic_version(3, 1, 0)
-        end
-
-        # Read the term program environment variable
-        #
-        # @example
-        #   iterm.term_program
-        #   # => "iTerm.app"
-        #
-        # @return [String, nil]
-        #
-        # @api private
-        def term_program
-          env[TERM_PROGRAM]
-        end
-
-        # Read the term program version environment variable
-        #
-        # @example
-        #   iterm.term_program_version
-        #   # => "1.2.3"
-        #
-        # @return [String, nil]
-        #
-        # @api private
-        def term_program_version
-          env[TERM_PROGRAM_VERSION]
         end
       end # Iterm
     end # Terminals
