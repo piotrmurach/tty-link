dbf199fd6b2a07398bfde09b12a2b542b7654dc3
Piotr Murach <pmurach@gmail.com>
2026-07-17 21:39:10 +0200

    Change the RSpec configuration to remove blank lines

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,9 +21,7 @@ require "tty-link"
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
-
   config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.max_formatted_output_length = nil
     c.syntax = :expect
