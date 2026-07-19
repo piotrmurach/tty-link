a3c810ed3c9279ab995aad0bfb2bfa487e7ed2e2
Piotr Murach <pmurach@gmail.com>
2026-07-19 21:15:07 +0200

    Change the RSpec configuration to remove the expectation syntax setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,6 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
   config.expect_with :rspec do |c|
     c.max_formatted_output_length = nil
-    c.syntax = :expect
   end
   config.disable_monkey_patching!
 end
