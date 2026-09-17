8a84982fc395a2297a639bb924403f83cebcd80e
Piotr Murach <pmurach@gmail.com>
2026-09-17 21:54:52 +0200

    Add the Abstract class test for accessing the missing term program env

--- spec/unit/terminals/abstract_spec.rb
+++ spec/unit/terminals/abstract_spec.rb
@@ -149,6 +149,17 @@ RSpec.describe TTY::Link::Terminals::Abstract do
 
       expect(terminal.name).to eq("terminal")
     end
+
+    it "accesses the missing term program environment variable in a subclass" do
+      stub_const("Terminal", Class.new(described_class) do
+        def name
+          term_program
+        end
+      end)
+      terminal = Terminal.new(semantic_version, {})
+
+      expect(terminal.name).to eq(nil)
+    end
   end
 
   describe "#term_program_version" do
